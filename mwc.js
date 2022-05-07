cd50eaf3f2385e75e34bdf20fc08f6ace6600453
Chuma Takahiro <chuma@unicorn.limited>
2022-05-07 10:05:04 +0900

    add @material 0.25.3 build

---
+++ mwc.js
@@ -0,0 +1,14 @@
+import '@material/mwc-button';
+import '@material/mwc-dialog';
+import '@material/mwc-fab';
+import '@material/mwc-formfield';
+import '@material/mwc-linear-progress';
+import '@material/mwc-list';
+import '@material/mwc-list/mwc-list-item';
+import '@material/mwc-menu';
+import '@material/mwc-ripple';
+import '@material/mwc-select';
+import '@material/mwc-snackbar';
+import '@material/mwc-switch';
+import '@material/mwc-textarea';
+import '@material/mwc-textfield';
