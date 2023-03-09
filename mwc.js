cbffc7525fe5f9ba64358003fd23499614f520eb
Chuma Takahiro <chuma@unicorn.limited>
2023-03-09 18:21:29 +0900

    remove unused mwc-fab/mwc-swtch

--- mwc.js
+++ mwc.js
@@ -26,7 +26,6 @@ import '@material/web/textfield/outlined-text-field.js';
 // Classic mwc components
 import '@material/mwc-button';
 import '@material/mwc-dialog';
-import '@material/mwc-fab';
 import '@material/mwc-formfield';
 import '@material/mwc-linear-progress';
 import '@material/mwc-list';
@@ -34,6 +33,5 @@ import '@material/mwc-list/mwc-list-item';
 import '@material/mwc-menu';
 import '@material/mwc-ripple';
 import '@material/mwc-snackbar';
-import '@material/mwc-switch';
 import '@material/mwc-textarea';
 import '@material/mwc-textfield';
