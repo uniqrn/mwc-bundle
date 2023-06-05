3149f2bc03e7f768addd8a02643757398865950d
Chuma Takahiro <chuma@unicorn.limited>
2023-06-05 13:20:49 +0000

    upgrade material-web pre9 / remove mwc-dialog, mwc-linear-progress

--- mwc.js
+++ mwc.js
@@ -1,18 +1,12 @@
 // Buttons
 import '@material/web/button/elevated-button.js';
-import '@material/web/button/elevated-link-button.js';
 import '@material/web/button/filled-button.js';
-import '@material/web/button/filled-link-button.js';
 import '@material/web/button/outlined-button.js';
-import '@material/web/button/outlined-link-button.js';
 import '@material/web/button/text-button.js';
-import '@material/web/button/text-link-button.js';
 import '@material/web/button/tonal-button.js';
-import '@material/web/button/tonal-link-button.js';
 import '@material/web/icon/icon.js';
 import '@material/web/iconbutton/standard-icon-button.js';
 import '@material/web/fab/fab.js';
-import '@material/web/fab/fab-extended.js';
 // List
 import '@material/web/list/list.js';
 import '@material/web/list/list-item.js';
@@ -20,13 +14,11 @@ import '@material/web/list/list-item-link.js';
 // Others
 import '@material/web/switch/switch.js';
 import '@material/web/dialog/dialog.js';
+import '@material/web/linearprogress/linear-progress.js';
 import '@material/web/textfield/filled-text-field.js';
 import '@material/web/textfield/outlined-text-field.js';
 
 // Classic mwc components
-import '@material/mwc-button';
-import '@material/mwc-dialog';
-import '@material/mwc-linear-progress';
 import '@material/mwc-list';
 import '@material/mwc-list/mwc-list-item';
 import '@material/mwc-menu';
