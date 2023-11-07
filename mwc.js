84566ccf98411a009106a1807eadbf56ac81a50f
Chuma Takahiro <chuma@unicorn.limited>
2023-11-07 16:41:08 +0900

    add md-chips

--- mwc.js
+++ mwc.js
@@ -21,6 +21,11 @@ import '@material/web/select/filled-select.js';
 import '@material/web/select/outlined-select.js';
 import '@material/web/select/select-option.js';
 // Others
+import '@material/web/chips/chip-set.js';
+import '@material/web/chips/assist-chip.js';
+import '@material/web/chips/filter-chip.js';
+import '@material/web/chips/input-chip.js';
+import '@material/web/chips/suggestion-chip.js';
 import '@material/web/checkbox/checkbox.js';
 import '@material/web/radio/radio.js';
 import '@material/web/switch/switch.js';
