ab3f8a475af0a7395a7d29baaa98f1359cd98072
Chuma Takahiro <chuma@unicorn.limited>
2025-04-28 12:51:12 +0900

    add @maicol07/material-web-additions/snackbar

--- mwc.js
+++ mwc.js
@@ -40,5 +40,8 @@ import '@material/web/tabs/tabs.js';
 import '@material/web/tabs/primary-tab.js';
 import '@material/web/tabs/secondary-tab.js';
 
+// Material Web Additions
+import '@maicol07/material-web-additions/snackbar/snackbar.js';
+
 // Classic mwc components
 import '@material/mwc-snackbar';
